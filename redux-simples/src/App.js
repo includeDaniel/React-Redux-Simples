4820da8eef975334784067ae5e2853e845ace700
Daniel Nunes <danielzcomputacao@gmail.com>
2021-05-11 15:46:53 -0300

    colors

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <h1>Exercício React-Redix (Simples)</h1>
+      <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Card 1" red>
+          X
+          </Card>
+        <Card title="Card 2" green>
+          X
+          </Card>
+      </div>
+      <div className="linha">
+        <Card title="Card 3" blue>
+          Y
+          </Card>
+        <Card title="Card 4" purple>
+          Y
+          </Card>
       </div>
     </div>
   );
--- redux-simples/src/components/Card.jsx
+++ redux-simples/src/components/Card.jsx
@@ -1,6 +1,23 @@
 import "./Card.css";
 import React from "react";
 
+// eslint-disable-next-line
+function getColor(props) {
+  if (props.red) return "Red";
+  if (props.green) return "Green";
+  if (props.blue) return "Blue";
+  if (props.purple) return "Purple";
+  return "";
+}
+
+// eslint-disable-next-line
 export default (props) => {
-  return <div>Card</div>;
+  return (
+    <div className={`Card ${getColor(props)}`}>
+      <div className="Header">
+        <span className="Title">{props.title}</span>
+      </div>
+      <div className="Content">{props.children}</div>
+    </div>
+  );
 };
