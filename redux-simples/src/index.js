c025be93c14927ff75c836bbc556237b3384806d
Daniel Nunes <danielzcomputacao@gmail.com>
2021-05-13 16:28:15 -0300

    redux

--- redux-simples/src/index.js
+++ redux-simples/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import configStore from './store/storeConfig'
 
+const store = configStore()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ redux-simples/src/store/storeConfig.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers } from 'redux'
+
+const reducers = combineReducers({
+    numeros: function (state, action) {
+        console.log(state, ' ', action)
+        return {
+            min: 1,
+            max: 10
+        }
+    },
+    nomes: function (state, action) {
+        return [
+            'Ana',
+            'Bia',
+            'Carlos'
+        ]
+    }
+})
+
+function storeConfig() {
+    return createStore(reducers)
+}
+
+export default storeConfig
